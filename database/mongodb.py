2398b606a29b8b65f860eeef5936aa1cb30085f2
Ancientmemer <ancientmemer12@gmail.com>
2026-01-09 10:35:21 +0530

    Refactor MongoDB collection and update functions

--- database/mongodb.py
+++ database/mongodb.py
@@ -2,19 +2,31 @@ from pymongo import MongoClient
 from config.config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
-db = client["auto_message_bot"]
-collection = db["schedules"]
+db = client["auto_scheduler"]
+collection = db["messages"]
+
 
 def add_message(data):
-    return collection.insert_one(data).inserted_id
+    collection.insert_one(data)
 
-def get_all(chat_id=None):
-    if chat_id is None:
-        return list(collection.find({}))
-    return list(collection.find({"chat_id": chat_id}))
+
+def get_all(chat_id):
+    return list(collection.find({"chat_id": chat_id, "status": "running"}))
+
 
 def get_all_running():
     return list(collection.find({"status": "running"}))
 
+
 def stop_message(msg_id):
-    collection.update_one({"_id": msg_id}, {"$set": {"status": "stopped"}})
+    collection.update_one(
+        {"_id": msg_id},
+        {"$set": {"status": "stopped"}}
+    )
+
+
+def update_last_sent(msg_id, timestamp):
+    collection.update_one(
+        {"_id": msg_id},
+        {"$set": {"last_sent": timestamp}}
+    )
