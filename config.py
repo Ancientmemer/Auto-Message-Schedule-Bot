09df7a8a414d15d52c20119e13a0d518f9880566
Ancientmemer <ancientmemer12@gmail.com>
2026-01-09 11:05:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH")
+
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
