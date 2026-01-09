725eeb26f90fca0a019ae2ef4ff931bfac71b8b7
Ancientmemer <ancientmemer12@gmail.com>
2026-01-09 11:26:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,21 @@
+from pyrogram import Client, filters, idle
 import asyncio
-from pyrogram import Client, idle, filters
-from config import BOT_TOKEN, API_ID, API_HASH
+import os
 
 app = Client(
-    "AutoMessageBot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    "test",
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    bot_token=os.getenv("BOT_TOKEN")
 )
 
 @app.on_message(filters.text)
-async def echo(client, message):
-    print("MESSAGE RECEIVED")
-    await message.reply("👀 I am alive")
+async def echo(_, msg):
+    await msg.reply("✅ I am alive")
 
 async def main():
     await app.start()
-    print("🤖 Bot started")
+    print("BOT STARTED")
     await idle()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
